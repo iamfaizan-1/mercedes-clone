8657817c71f8b8d75b33ff0a9d0b6abf0583b53f
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-11-14 22:16:52 +0500

    navbar added

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let navbar2 = document.querySelector(".nav2")
+
+navbar2.addEventListener("mouseover",() =>{
+    navbar2.style.color = "#9f9f9f"
+    navbar2.style.transition = "color 0.3s ease"
+})
+
+navbar2.addEventListener("mouseleave",() =>{
+    navbar2.style.color = "white"
+    navbar2.style.transition = "color 0.5s ease"
+})
+
